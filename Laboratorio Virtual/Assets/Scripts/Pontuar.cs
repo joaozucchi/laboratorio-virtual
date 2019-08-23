fd577cc2acb880d45e7b14a47615ff3a92bdd92f
joaozucchi <joaohercilio@hotmail.com>
2019-08-22 23:32:14 -0300

    Fases funcionando sem placar

--- Laboratorio Virtual/Assets/Scripts/Pontuar.cs
+++ Laboratorio Virtual/Assets/Scripts/Pontuar.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
 
 public class Pontuar : MonoBehaviour
 {
+    public Text texto;
     public void PontuarQuestao()
     {
         PlayerPrefs.SetInt("Pontos", PlayerPrefs.GetInt("Pontos") + 1);
@@ -12,6 +16,11 @@ public class Pontuar : MonoBehaviour
     public void ZerarPontuacao()
     {
         PlayerPrefs.SetInt("Pontos", 0);
+    }
+    
+    public void MostrarPlacar()
+    {
+        texto.text = "Você acertou" + PlayerPrefs.GetInt("Pontos") + " de 3 questões!";
     }
 
 }
