35768813a685423730a62e4497d61625c20cfab2
joaozucchi <joaohercilio@hotmail.com>
2019-09-02 21:28:07 -0300

    marcação adicionada

--- Laboratorio Virtual/Assets/Scripts/TouchVidraria.cs
+++ Laboratorio Virtual/Assets/Scripts/TouchVidraria.cs
@@ -6,7 +6,7 @@ public class TouchVidraria : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    public GameObject botaoCorreto, botaoIncorreto;
+    public GameObject botaoCorreto, botaoIncorreto, seta;
     public string VidrariaCorreta;
     string vidraria;
     void Start()
@@ -23,18 +23,45 @@ public class TouchVidraria : MonoBehaviour
             RaycastHit Hit;
             if(Physics.Raycast(ray, out Hit))
             {
+                
                 vidraria = Hit.transform.name;
+                GameObject objeto = GameObject.FindGameObjectWithTag(vidraria);
+                seta.transform.position = new Vector3(objeto.transform.position.x, seta.transform.position.y, objeto.transform.position.z);
+                seta.SetActive(true);
 
                 if (Equals(vidraria, VidrariaCorreta))
                 {
                     botaoCorreto.SetActive(true);
+                    botaoIncorreto.SetActive(false);
                 }
                 else
                 {
                     botaoIncorreto.SetActive(true);
+                    botaoCorreto.SetActive(false);
                 }
             }
         }
     }
+
+    /*public void teste()
+    {
+        string v = "BalaodefundoRedondo";
+        GameObject objeto = GameObject.FindGameObjectWithTag(v);
+        seta.transform.position = new Vector3(objeto.transform.position.x, seta.transform.position.y, objeto.transform.position.z);
+        seta.SetActive(true);
+
+        if (Equals(v, VidrariaCorreta))
+        {
+            botaoCorreto.SetActive(true);
+            botaoIncorreto.SetActive(false);
+        }
+        else
+        {
+            botaoIncorreto.SetActive(true);
+            botaoCorreto.SetActive(false);
+        }
+    }*/
+
+
 }
 //https://www.youtube.com/watch?v=hi_KDpC1nzk
