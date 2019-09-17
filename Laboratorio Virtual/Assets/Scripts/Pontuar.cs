a0570938e34c9071a561b0637af9364eae6b101b
77larsonkv@gmail.com <Minhaguitarra77>
2019-09-17 09:58:13 -0300

    Lista de Respostas

--- Laboratorio Virtual/Assets/Scripts/Pontuar.cs
+++ Laboratorio Virtual/Assets/Scripts/Pontuar.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
 
@@ -8,20 +9,37 @@ using TMPro;
 public class Pontuar : MonoBehaviour
 {
     public TextMeshProUGUI texto;
+    public string[] nomeFases;
+    
     public void PontuarQuestao()
     {
         PlayerPrefs.SetInt("Pontos", PlayerPrefs.GetInt("Pontos") + 1);
-        Debug.Log("ESSE EH O VALOR: " + PlayerPrefs.GetInt("Pontos"));
+        string name = SceneManager.GetActiveScene().name;
+        PlayerPrefs.SetInt(name, 1);
     }
 
     public void ZerarPontuacao()
     {
-        PlayerPrefs.SetInt("Pontos", 0);
+        PlayerPrefs.DeleteAll();
     }
     
     public void MostrarPlacar()
     {
-        texto.text = "Você acertou " + PlayerPrefs.GetInt("Pontos") + " de 3 questões!";
-    }
 
+        for (int i = 0; i < nomeFases.Length; i++)
+        {
+            string a;
+            if(PlayerPrefs.GetInt(nomeFases[i]) == 1)
+            {
+                a = "Correta";
+            }
+            else
+            {
+                a = "Incorreta";
+            }
+            int x = i + 1;
+            texto.text += "Questão" + x + ": " + a +"<br>";
+        }
+        texto.text += "Você acertou " + PlayerPrefs.GetInt("Pontos") + " de 3 questões! <br>";
+    }
 }
