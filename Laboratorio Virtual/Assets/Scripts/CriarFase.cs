9108a66840ebdfe92914e58414f534c773dce81e
Larsonkv <50681729+Larsonkv@users.noreply.github.com>
2019-11-12 11:06:23 -0300

    Criar fase

---
+++ Laboratorio Virtual/Assets/Scripts/CriarFase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+using UnityEditor.SceneManagement;
+
+public  class CriarFase : MonoBehaviour
+{
+    public  Text v1, v2, v3, vCorreta;
+    public TextMeshProUGUI perguntaText;
+
+    public void Criar()
+    {
+        Fase.pergunta = perguntaText.text;
+        Fase.v1 = v1.text;
+        Fase.v2 = v2.text;
+        Fase.v3 = v3.text;
+        Fase.vCorreta = vCorreta.text;
+        EditorSceneManager.LoadScene("faseTeste");
+    }
+}
---
+++ Laboratorio Virtual/Assets/Scripts/Fase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public static class Fase 
+{
+    public static string pergunta, v1, v2, v3, vCorreta;
+}
